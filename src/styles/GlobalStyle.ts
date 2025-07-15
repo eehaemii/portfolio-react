e1fd32d216e093bb4a0fbbef7637de6c551b0e7e
eehaemi <eehaemii@gmail.com>
2025-07-15 16:24:05 +0900

    scrollbar style add

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,6 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+ /* Webkit */
+  ::-webkit-scrollbar-thumb {
+    background: rgba(0,0,0,0.5);
+    border-radius: 4px;
+  }
+
+  /* Firefox */
+  * {
+    scrollbar-width: thin;
+    scrollbar-color: rgba(0,0,0,0.5) transparent;
+  }
+
   body {
     font-family: Pretendard, -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", sans-serif;
   }
